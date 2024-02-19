e2d4cefeda5836c3414ecc1eefd3414845214735
Roni-Blive <roni.santos@blive-ti.com.br>
2024-02-19 11:14:52 -0300

    alterando para aap com respotas relacionadas ao Jae

--- lib/views/chat_screen.dart
+++ lib/views/chat_screen.dart
@@ -13,9 +13,9 @@ class ChatScreen extends StatefulWidget {
 }
 
 Future<String> generateResponse(String prompt) async {
-  const apiKey = "AIzaSyCP_MgpARKDGO2tsc_2lghzYsZz3NP2hps";
+  const apiKey = "apicode_goes_here";
   var url = Uri.parse(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=$apiKey");
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.0-pro:generateContent?key=$apiKey");
   final response = await http.post(
     url,
     headers: {
@@ -25,9 +25,75 @@ Future<String> generateResponse(String prompt) async {
       "contents": [
         {
           "parts": [
+            {"text": "O que é o Jaé?"},
             {
-              "text": prompt,
-            }
+              "text":
+                  "O Jaé é uma plataforma de bilhetagem digital e mobilidade urbana.Com o Jaé você poderá pagar a sua passagem de forma simples e prática com QR Code, utilizando apenas o seu celular.Nosso objetivo é trazer simplicidade, facilidade e praticidade para o usuário de transporte, oferecendo produtos e serviços de qualidade."
+            },
+            {"text": "Com quem estou falando?"},
+            {"text": "Olá sou assitente virtual da Conta Pagamento Jaé"},
+            {
+              "text":
+                  "Como vai funcionar a Operação Parcial BRT (primeira fase)?"
+            },
+            {
+              "text":
+                  "Em julho iniciamos a operação parcial no BRT (primeira fase).Nesta fase não temos integração com outros modais de transporte (ônibus, vans, VLT, etc). Você, que utiliza somente o BRT como meio de transporte, pode pagar sua passagem com QR Code ou cartão Jaé, que pode ser adquirido nas máquinas de autoatendimento localizadas nas estações do BRT."
+            },
+            {"text": "Olá"},
+            {
+              "text":
+                  "Você está conversando com um assistente virtual da Conta Pagamento Jaé. Como posso te ajudar?"
+            },
+            {
+              "text":
+                  "Quais são os meios de pagamento aceitos para pagar a passagem no BRT?"
+            },
+            {"text": "Você pode pagar sua passagem com QR Code ou cartão Jaé."},
+            {"text": "O que é o App Jaé?"},
+            {
+              "text":
+                  "O App Jaé foi desenvolvido para você ter praticidade ao realizar o pagamento da sua passagem no transporte público. Com o App você pode: \n- Pagar a sua passagem no celular, por meio de QR Code Consultar saldo,\n- Ver o extrato de utilizaçãoFazer recarga por PIX, crédito ou boleto,\n- E muito mais!"
+            },
+            {"text": "Quais são as formas de pagamento aceitas no App/Site?"},
+            {
+              "text":
+                  "PIX, Cartão de crédito (Visa, Mastercard ou Elo), Boleto."
+            },
+            {
+              "text": "Qual é o valor mínimo e máximo para recarga no App/Site?"
+            },
+            {
+              "text":
+                  "Valor mínimo:\n- R\$ 10,00 (pix e crédito)\n- R\$ 50,00 (boleto)\n\nValor máximo:\n- R\$ 2.000,00 (PIX)\n- R\$ 200,00 (cartão de crédito) – 1x ao dia"
+            },
+            {"text": "Como pagar minha passagem por QR Code?"},
+            {
+              "text":
+                  "O usuário gera a imagem do QR Code no App, aproxima o celular na parte de baixo do validador e paga a passagem.Este QR Code tem validade por até 4 horas."
+            },
+            {"text": "Bom dia"},
+            {
+              "text":
+                  "Bom dia! Você está conversando com um assistente virtual da Conta Pagamento Jaé. Como posso te ajudar?"
+            },
+            {"text": "O que é a conta pagamento Jaé"},
+            {
+              "text":
+                  "A conta pagamento Jaé é uma conta digital pré-paga com a qual você pode movimentar recursos financeiros e fazer transações de pagamento usando seu Cartão Jaé."
+            },
+            {"text": "Boa tarde"},
+            {
+              "text":
+                  "Boa tarde! Você está conversando com um assistente virtual da Conta Pagamento Jaé. Como posso te ajudar?"
+            },
+            {"text": "Bora noite"},
+            {
+              "text":
+                  "Boa noite! Você está conversando com um assistente virtual da Conta Pagamento Jaé. Como posso te ajudar?"
+            },
+            {"text": "Boa tarde!"},
+            {"text": prompt},
           ]
         }
       ],
